3741c7314fece94030951e113503d369d4b556da
Jonah <gatesj@oregonstate.edu>
2024-08-26 12:42:34 -0700

    New route for allowing user to upload a populated csv file, which iteratively adds each row and its attributes as a single ite to the database.

--- app.py
+++ app.py
@@ -39,8 +39,32 @@ def download_template():
     with open(csv_file, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['item_name', 'item_description'])
-    return send_file(csv_file, as_attached=True)
+    return send_file(csv_file)
 
+#For uploading populated csv, should start at index 1 skipping first row. 
+@app.route("/upload_csv", methods=["POST"])
+def upload_csv():
+        if "file" not in request.files:
+            return "No file part in form.", 400
+        file = request.files["file"]
+        if file.filename == '':
+            return "No selected file.", 400
+        if file and file.filename.endswith(".csv"):
+            file_data = file.stream.read().decode().splitlines()
+            rows = csv.reader(file_data)
+            # Effectively skipping the first row which is a header for the table
+            next(rows)
+            for row in rows:
+                if len(row) < 2:
+                    continue
+                item_name = row[0]
+                item_description = row[1]
+                add_item(item_name, item_description, item_availability="Available")
+            return view_items_page()    
+            #return "Items added successfully.", 200
+        #return "Invalid file format.", 400
+        return view_register_item_page()
+        
 if __name__ == '__main__':
     app.run(debug=True)
 
